2c6bd1eb928c98b044fa2e64b79079a5fcecda8d
Joshua Lent <Joshualent83@gmail.com>
2025-03-28 15:42:24 -0500

    Add user list view

--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,16 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 from .forms import CustomUserCreationForm
 
+
 class SignupView(CreateView):
-    
-    '''Signup view'''
+    """Signup view"""
+
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
+
 
 class TestView(ListView):
-    
-    success_url = reverse_lazy('test_template')
-    template_name = 'test_template.html'
+
+    success_url = reverse_lazy("test_template")
+    template_name = "test_template.html"
