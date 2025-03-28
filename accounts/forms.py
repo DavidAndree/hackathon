fe17a903d7d2e3ce32c2d4ea08480cede82c9e04
Joshua Lent <joshualent83@gmail.com>
2025-03-28 10:53:44 -0500

    Fix login/signup functionality and update requirements.txt

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,16 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from.models import CustomUser
+from .models import CustomUser
+
 
 class CustomUserCreationForm(UserCreationForm):
-    '''Custom user creation form'''
-    
+    """Custom user creation form"""
+
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = ('username', 'email', 'age',)
-        
+        fields = ("username", "email", "age", "city", "state")
+
+
 class CustomUserChangeForm(UserChangeForm):
-    '''Custom user change form'''
-    
+    """Custom user change form"""
+
     class Meta:
         model = CustomUser
-        fields = ('username', 'email', 'age',)
+        fields = ("username", "email", "age", "city", "state")
