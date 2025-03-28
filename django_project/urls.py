3c7a0e09af97cdf5882e86689bf688a95e9df7bc
DavidAndree <davidalvaradoc0106@gmail.com>
2025-03-28 17:15:13 -0500

    Modify django_project/urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,20 +20,10 @@ from django.views.generic import TemplateView, ListView
 from accounts.models import CustomUser
 
 urlpatterns = [
-<<<<<<< HEAD
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('message/', include('messaging.urls')),
     path('mentorships/', include ('mentorships.urls')),
-=======
-    path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path(
-        "", ListView.as_view(model=CustomUser, template_name="home.html"), name="home"
-    ),
-    path("message/", include("messaging.urls")),
->>>>>>> refs/remotes/origin/main
 ]
