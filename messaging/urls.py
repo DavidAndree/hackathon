a67af0bc216313073c64caacda3f590968b27325
Arriza Wibowo <arrizafathi@gmail.com>
2025-03-28 15:57:41 -0400

    Finish messaging functionality

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import ConversationView, AddMessageView
 
+app_name = 'messaging'  # Namespacing the app
+
 urlpatterns = [
-    path('message/list/', ConversationView.as_view(), name='conversation'),
-    path('message/new/', AddMessageView.as_view(), name='add_message'),
+    path('', ConversationView.as_view(), name='conversation'),  # Access via /message/
+    path('new/', AddMessageView.as_view(), name='add_message'),  # Access via /message/new/
 ]
--- messaging/views.py
+++ messaging/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Message
 from django.urls import reverse_lazy
+from .models import Message
 from .forms import MessageForm
 
 class ConversationView(LoginRequiredMixin, ListView):
@@ -14,7 +14,7 @@ class AddMessageView(LoginRequiredMixin, CreateView):
     model = Message
     form_class = MessageForm
     template_name = "messaging/add_message.html"
-    success_url = reverse_lazy("conversation")
+    success_url = reverse_lazy("messaging:conversation")  # Fix: Use namespace
 
     def form_valid(self, form):
         form.instance.sender = self.request.user  # Automatically set sender
