441b6d6348828c9baec59eb99450ffeef79425ac
DavidAndree <davidalvaradoc0106@gmail.com>
2025-03-28 14:05:33 -0500

    Add folder config pov and add url for mentor-config.html

--- mentorships/urls.py
+++ mentorships/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ConfigureRoleView, MenteeDashboardView, MentorDashboardView
+from .views import ConfigureRoleView, MenteeDashboardView, MentorDashboardView, MentorConfigView
 
 urlpatterns = [
     path('configure-role/', ConfigureRoleView.as_view(), name='configure_role'),
     path('mentee-dashboard/', MenteeDashboardView.as_view(), name='mentee_dashboard'),
     path('mentor-dashboard/', MentorDashboardView.as_view(), name='mentor_dashboard'),
+    path('mentor-config/',MentorConfigView.as_view(), name='mentor_dashboard'),
 ]
